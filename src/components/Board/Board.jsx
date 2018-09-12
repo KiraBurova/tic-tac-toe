6b848e10255595bb07c39bbba560e61585a6157e
Kira <kira@nativeroll.tv>
2018-09-12 11:51:19 +0300

    work on components, add redux

---
+++ src/components/Board/Board.jsx
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+
+import Cell from '../Cell/Cell';
+
+class Board extends Component {
+    render() {
+       return <div>div is here</div>
+    }
+}
+export default Board;
